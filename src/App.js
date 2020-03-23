57633da90634bd14ab43044270371c2669ec5486
seungyong88 <gosng2001@naver.com>
2020-03-23 14:53:24 +0900

    feat 카운터 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
-    <div className="App">
-      GOGO
-    </div>
+    <CounterContainer />
   );
 }
 
