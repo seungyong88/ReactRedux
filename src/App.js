1805cbab6c2f8e0c20fb0fe4bd004adaf01adca9
seungyong88 <gosng2001@naver.com>
2020-03-23 15:07:34 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
-    <CounterContainer />
+    <div>
+      test
+    </div>
   );
 }
 
